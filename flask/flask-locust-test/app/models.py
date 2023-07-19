6848b2578db2382bcfe38f70c31f8ad3f7c6b49e
Cristiano S. Oliveira <cristianounix@gmail.com>
2023-07-18 22:12:34 -0300

    Flask locust - Config and models

---
+++ flask/flask-locust-test/app/models.py
@@ -0,0 +1,27 @@
+from typing import Dict, Union
+
+from app.app import db
+from sqlalchemy import ForeignKey
+
+
+class Author(db.Model):
+    __tablename__ = "authors"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+    def dict(self) -> Dict[str, Union[int, str]]:
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+        }
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    author_id = db.Column(db.Integer, ForeignKey("authors.id"), nullable=False)
+    title = db.Column(db.Text)
+    original_language = db.Column(db.Text)
+    isbn = db.Column(db.Text)
